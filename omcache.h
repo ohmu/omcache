b673821f6a236044bc261c5e506c5a62f05a3d96
Oskari Saarenmaa <os@ohmu.fi>
2014-11-04 00:18:37 +0200

    omcache.h: add extern "C" for c++ use and drop unused includes
    OMcache no longer uses any vararg functions so stdarg.h isn't needed.
    stddef.h is not needed either as all types we use are in sys/types.h.

--- omcache.h
+++ omcache.h
@@ -12,12 +12,18 @@
 #ifndef _OMCACHE_H
 #define OMCACHE_H 1
 
-#include <stdarg.h>
-#include <stddef.h>
 #include <stdint.h>
 #include <sys/types.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
 #include "omcache_cdef.h"
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
 
 // CFFI can't handle defines yet
 #define OMCACHE_VERSION 0x00000100  // Version 0.1.0
