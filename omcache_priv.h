95377a5e4e0f5e2c9968985a065b8711d813a638
Oskari Saarenmaa <os@ohmu.fi>
2014-11-21 23:33:37 +0200

    util: generalized growable hash table for pointers

--- omcache_priv.h
+++ omcache_priv.h
@@ -19,26 +19,35 @@
 
 #define omc_hidden __attribute__((visibility("hidden")))
 
-typedef struct omc_int_hash_node_s
+typedef struct omc_hash_node_s
 {
-  int key;
-  int val;
-  struct omc_int_hash_node_s *next;
-} omc_int_hash_node_t;
+  uint32_t key;
+  void *val;
+  struct omc_hash_node_s *next;
+} omc_hash_node_t;
 
-typedef struct omc_int_hash_table_s
+typedef struct omc_hash_table_s
 {
   uint32_t size;
-  omc_int_hash_node_t **buckets;
-  omc_int_hash_node_t *freelist;
-  omc_int_hash_node_t nodes[];
-} omc_int_hash_table_t;
+  uint32_t count;
+  void *not_found_val;
+  omc_hash_node_t **buckets;
+  omc_hash_node_t *freelist;
+  omc_hash_node_t nodes[];
+} omc_hash_table_t;
 
-omc_hidden omc_int_hash_table_t *omc_int_hash_table_init(omc_int_hash_table_t *hash, uint32_t size);
-omc_hidden void omc_int_hash_table_free(omc_int_hash_table_t *hash);
-omc_hidden int omc_int_hash_table_find(omc_int_hash_table_t *hash, int key);
-omc_hidden int omc_int_hash_table_add(omc_int_hash_table_t *hash, int key, int val);
-omc_hidden int omc_int_hash_table_del(omc_int_hash_table_t *hash, int key);
+omc_hidden omc_hash_table_t *omc_hash_table_init(omc_hash_table_t *hash, uint32_t size, void *not_found_val);
+omc_hidden void omc_hash_table_free(omc_hash_table_t *hash);
+omc_hidden void *omc_hash_table_find(omc_hash_table_t *hash, uint32_t key);
+omc_hidden int omc_hash_table_add(omc_hash_table_t *hash, uint32_t key, void *val);
+omc_hidden void *omc_hash_table_del(omc_hash_table_t *hash, uint32_t key);
+
+#define omc_int_hash_table_t omc_hash_table_t
+#define omc_int_hash_table_init(h,s) omc_hash_table_init((h), (s), (void *) (uintptr_t) -1)
+#define omc_int_hash_table_free(h) omc_hash_table_free(h)
+#define omc_int_hash_table_find(h,k) ((intptr_t) omc_hash_table_find((h), (k)))
+#define omc_int_hash_table_add(h,k,v) omc_hash_table_add((h), (k), (void *) (uintptr_t) (v))
+#define omc_int_hash_table_del(h,k) omc_hash_table_del((h), (k))
 
 omc_hidden void omc_hash_md5(const unsigned char *key, size_t key_len, unsigned char *buf);
 omc_hidden uint32_t omc_hash_jenkins_oat(const unsigned char *key, size_t key_len);
--- util.c
+++ util.c
@@ -25,89 +25,93 @@ omc_hash_uint32(uint32_t val)
   return val;
 }
 
-static void omc_int_hash_table_reset(omc_int_hash_table_t *hash)
+static omc_hash_table_t *omc_hash_table_reset(omc_hash_table_t *hash)
 {
-  memset(hash->buckets, 0, hash->size * sizeof(omc_int_hash_node_t *));
+  hash->count = 0;
   hash->freelist = NULL;
   for (uint32_t i = 0; i < hash->size; i ++)
     {
+      hash->buckets[i] = NULL;
       hash->nodes[i].key = -1;
-      hash->nodes[i].val = -1;
+      hash->nodes[i].val = hash->not_found_val;
       hash->nodes[i].next = hash->freelist;
       hash->freelist = &hash->nodes[i];
     }
+  return hash;
 }
 
-omc_int_hash_table_t *omc_int_hash_table_init(omc_int_hash_table_t *old_hash, uint32_t size)
+omc_hash_table_t *omc_hash_table_init(omc_hash_table_t *old_hash, uint32_t size, void *not_found_val)
 {
   // reuse old hash table if one was given and the size is right
   if (old_hash != NULL)
     {
-      if (old_hash->size >= size && old_hash->size < size * 2)
-        {
-          omc_int_hash_table_reset(old_hash);
-          return old_hash;
-        }
-      omc_int_hash_table_free(old_hash);
+      if (old_hash->size >= size)
+        return omc_hash_table_reset(old_hash);
+      omc_hash_table_free(old_hash);
     }
   // allocate the hash table and enough memory to hold all the nodes and
   // pointers to bucket heads in a single allocation
-  size_t struct_alloc = sizeof(omc_int_hash_table_t);
-  size_t nodes_alloc = size * sizeof(omc_int_hash_node_t);
-  size_t pointers_alloc = size * sizeof(omc_int_hash_node_t *);
-  omc_int_hash_table_t *hash = malloc(struct_alloc + nodes_alloc + pointers_alloc);
+  size_t struct_alloc = sizeof(omc_hash_table_t);
+  size_t nodes_alloc = size * sizeof(omc_hash_node_t);
+  size_t pointers_alloc = size * sizeof(omc_hash_node_t *);
+  omc_hash_table_t *hash = malloc(struct_alloc + nodes_alloc + pointers_alloc);
+  hash->not_found_val = not_found_val;
   hash->size = size;
-  hash->buckets = (omc_int_hash_node_t **) (
+  hash->buckets = (omc_hash_node_t **) (
     ((unsigned char *) hash) + struct_alloc + nodes_alloc);
-  omc_int_hash_table_reset(hash);
-  return hash;
+  return omc_hash_table_reset(hash);
 }
 
-void omc_int_hash_table_free(omc_int_hash_table_t *hash)
+void omc_hash_table_free(omc_hash_table_t *hash)
 {
   free(hash);
 }
 
-int omc_int_hash_table_find(omc_int_hash_table_t *hash, int key)
+void *omc_hash_table_find(omc_hash_table_t *hash, uint32_t key)
 {
   uint32_t bucket = omc_hash_uint32(key) % hash->size;
-  for (omc_int_hash_node_t *n = hash->buckets[bucket]; n != NULL; n = n->next)
+  for (omc_hash_node_t *n = hash->buckets[bucket]; n != NULL; n = n->next)
     if (n->key == key)
       return n->val;
-  return -1;
+  return hash->not_found_val;
 }
 
-int omc_int_hash_table_add(omc_int_hash_table_t *hash, int key, int val)
+int omc_hash_table_add(omc_hash_table_t *hash, uint32_t key, void *val)
 {
+  if (hash->count == hash->size)
+    return -1;
   uint32_t bucket = omc_hash_uint32(key) % hash->size;
-  omc_int_hash_node_t *n = hash->freelist;
+  omc_hash_node_t *n = hash->freelist;
   hash->freelist = n->next;
   n->next = hash->buckets[bucket];
   n->key = key;
   n->val = val;
   hash->buckets[bucket] = n;
+  hash->count ++;
   return 0;
 }
 
-int omc_int_hash_table_del(omc_int_hash_table_t *hash, int key)
+void *omc_hash_table_del(omc_hash_table_t *hash, uint32_t key)
 {
   uint32_t bucket = omc_hash_uint32(key) % hash->size;
-  omc_int_hash_node_t *p = NULL;
-  for (omc_int_hash_node_t *n = hash->buckets[bucket]; n != NULL; n = n->next)
+  omc_hash_node_t *p = NULL;
+  for (omc_hash_node_t *n = hash->buckets[bucket]; n != NULL; n = n->next)
     {
       if (n->key == key)
         {
+          void *val = n->val;
           if (p)
             p->next = n->next;
           else
             hash->buckets[bucket] = n->next;
-          n->next = hash->freelist;
           n->key = -1;
-          n->val = -1;
+          n->val = hash->not_found_val;
+          n->next = hash->freelist;
           hash->freelist = n;
-          return 0;
+          hash->count --;
+          return val;
         }
       p = n;
     }
-  return -1;
+  return hash->not_found_val;
 }
