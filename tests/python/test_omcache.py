34d33dfe719666bf6e63c3dd5eb4f61aeb5bbb2b
Oskari Saarenmaa <os@ohmu.fi>
2015-01-26 13:12:32 +0200

    omcache.py: more unit tests for commands
    All commands implemented in omcache.py should now be covered by unit tests
    although the tests don't test all possible functionality.  Timeouts are not
    tested by the python tests at all at the moment.

--- tests/python/test_omcache.py
+++ tests/python/test_omcache.py
@@ -1,7 +1,9 @@
 import errno
 import omcache
+import random
 import select
 from pytest import raises  # pylint: disable=E0611
+from time import sleep
 from . import OMcacheCase
 
 
@@ -28,6 +30,8 @@ class TestOmcache(OMcacheCase):
         assert s1 != s2
         assert len(s1) > 30
         assert len(s2) > 30
+        oc.noop(0)
+        oc.noop(1)
 
     def test_incr_decr(self):
         oc = omcache.OMcache([self.get_memcached()], self.log)
@@ -44,6 +48,11 @@ class TestOmcache(OMcacheCase):
         oc.set("test_incr_decr", "x567")
         with raises(omcache.DeltaBadValueError):
             oc.decrement("test_incr_decr", 5)
+        oc.set("test_incr_decr", "100")
+        assert oc.decrement("test_incr_decr", -5) == 105
+        assert oc.increment("test_incr_decr", -5) == 100
+        with raises(omcache.Error):
+            oc.increment("test_incr_decr_e", 2, expiration=42, initial=None)
 
     def test_add_replace_set_delete(self):
         oc = omcache.OMcache([self.get_memcached(), self.get_memcached()], self.log)
@@ -60,3 +69,137 @@ class TestOmcache(OMcacheCase):
         with raises(omcache.KeyExistsError):
             oc.add("test_arsd", "foobar")
         assert oc.get("test_arsd") == "replaced"
+        oc.delete("test_arsd")
+        with raises(omcache.NotFoundError):
+            oc.delete("test_arsd")
+        oc.set("test_arsd", "arsd", flags=531)
+        res, flags, cas = oc.get("test_arsd", flags=True, cas=True)  # pylint: disable=W0632
+        assert res == "arsd"
+        assert flags == 531
+        assert cas > 0
+        res, flags = oc.get("test_arsd", flags=True)  # pylint: disable=W0632
+        assert flags == 531
+
+    def test_cas(self):
+        oc = omcache.OMcache([self.get_memcached()], self.log)
+        with raises(omcache.NotFoundError):
+            oc.set("test_cas", "xxx", cas=42424242)
+        oc.set("test_cas", "xxx")
+        with raises(omcache.KeyExistsError):
+            oc.set("test_cas", "xxx", cas=42424242)
+        res, cas1 = oc.get("test_cas", cas=True)  # pylint: disable=W0632
+        assert res == "xxx"
+        assert cas1 > 0
+        oc.set("test_cas", "42", cas=cas1)
+        with raises(omcache.KeyExistsError):
+            oc.set("test_cas", "zzz", cas=cas1)
+        res, cas2 = oc.get("test_cas", cas=True)  # pylint: disable=W0632
+        assert res == "42"
+        assert cas2 > 0
+        assert cas2 != cas1
+        oc.increment("test_cas", 8)
+        with raises(omcache.KeyExistsError):
+            oc.set("test_cas", "zzz", cas=cas2)
+        res, cas3 = oc.get("test_cas", cas=True)  # pylint: disable=W0632
+        assert res == "50"
+        assert cas3 > 0
+        assert cas3 != cas2
+
+    def test_touch(self):
+        oc = omcache.OMcache([self.get_memcached()], self.log)
+        oc.set("test_touch", "qwerty", expiration=2)
+        assert oc.get("test_touch") == "qwerty"
+        sleep(2)
+        with raises(omcache.NotFoundError):
+            oc.get("test_touch")
+        oc.set("test_touch", "qwerty", expiration=1)
+        assert oc.get("test_touch") == "qwerty"
+        oc.touch("test_touch", expiration=3)
+        sleep(2)
+        assert oc.get("test_touch") == "qwerty"
+
+    def test_append_prepend(self):
+        oc = omcache.OMcache([self.get_memcached()], self.log)
+        oc.set("test_ap", "asdf")
+        assert oc.get("test_ap") == "asdf"
+        oc.append("test_ap", "zxcvb")
+        assert oc.get("test_ap") == "asdfzxcvb"
+        oc.prepend("test_ap", "qwerty")
+        assert oc.get("test_ap") == "qwertyasdfzxcvb"
+
+    def test_multi(self):
+        oc = omcache.OMcache([self.get_memcached(), self.get_memcached()], self.log)
+        item_count = 123
+        val = str(random.random())
+        for i in range(item_count):
+            oc.set("test_multi_{0}".format(i * 2), val, flags=i)
+        keys = ["test_multi_{0}".format(i) for i in range(item_count * 2)]
+        random.shuffle(keys)
+        results = oc.get_multi(keys)
+        assert len(results) == item_count
+        for i in range(item_count):
+            assert results["test_multi_{0}".format(i * 2)] == val
+        # test with flags and cas
+        results = oc.get_multi(keys, flags=True)
+        assert len(results) == item_count
+        for i in range(item_count):
+            assert results["test_multi_{0}".format(i * 2)] == (val, i)
+        results = oc.get_multi(keys, cas=True)
+        assert len(results) == item_count
+        # count the number of distinct cas values, we can't just compare
+        # them to the previous entry as we're using two memcache servers
+        # which may use the same cas values
+        casses = set()
+        for i in range(item_count):
+            res, cas = results["test_multi_{0}".format(i * 2)]
+            print "#{0} c{1}".format(i, cas)
+            assert res == val
+            casses.add(cas)
+        assert len(casses) > item_count / 3
+        results = oc.get_multi(keys, cas=True, flags=True)
+        assert len(results) == item_count
+        casses = set()
+        for i in range(item_count):
+            res, flags, cas = results["test_multi_{0}".format(i * 2)]
+            assert res == val
+            assert flags == i
+            casses.add(cas)
+        assert len(casses) > item_count / 3
+
+    def test_dist_methods(self):
+        # just make sure the different distribution methods distribute keys, well, differently
+        mc1 = self.get_memcached()
+        mc2 = self.get_memcached()
+        oc = omcache.OMcache([mc1, mc2], self.log)
+        item_count = 123
+        for i in range(item_count):
+            oc.set("test_dist_{0}".format(i), "orig_dist")
+        oc.flush()
+        oc.set_distribution_method("libmemcached_ketama")
+        for i in range(item_count):
+            oc.set("test_dist_{0}".format(i), "ketama")
+        oc.flush()
+        oc.set_distribution_method("libmemcached_ketama_weighted")
+        for i in range(item_count):
+            oc.set("test_dist_{0}".format(i), "ketama_weighted")
+        oc.flush()
+        oc.set_distribution_method("libmemcached_ketama_pre1010")
+        for i in range(item_count):
+            oc.set("test_dist_{0}".format(i), "ketama_pre1010")
+        oc.flush()
+        with raises(omcache.Error):
+            oc.set_distribution_method("xxx")
+        keys = ["test_dist_{0}".format(i) for i in range(item_count)]
+        oc.set_servers([mc1])
+        results = list(oc.get_multi(keys).values())
+        oc.set_servers([mc2])
+        results.extend(oc.get_multi(keys).values())
+        counts = {}
+        for value in results:
+            if value not in counts:
+                counts[value] = 0
+            counts[value] += 1
+        assert set(["ketama", "ketama_weighted", "ketama_pre1010"]).issuperset(counts)
+        assert counts["ketama"] >= item_count / 10
+        assert counts["ketama_weighted"] >= item_count / 10
+        assert counts["ketama_pre1010"] >= item_count / 10
