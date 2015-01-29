1814102298c73c0c5495045975d579b6dec88b22
Oskari Saarenmaa <os@ohmu.fi>
2015-01-29 11:46:07 +0200

    travis: test with multiple python versions
    Test CPython 2.6, 2.7, 3.3, 3.4 and PyPy 2.4 on Linux and just the C code on
    OS X as Travis doesn't currently support Python on OS X.
    
    Since the latest pylint versions no longer support Python 2.6 don't run it
    when testing with Python 2.6, but as we don't really need to run the full C
    unit test suite under Valgrind for all Python versions just run it when
    we're testing Python 2.6.

--- .travis.mk
+++ .travis.mk
@@ -3,10 +3,14 @@ all: $(shell uname -s)
 Linux:
 	sudo apt-get update
 	sudo apt-get install libasyncns-dev check memcached valgrind
-	sudo pip install cffi pytest pylint
+	pip install cffi pytest pylint
 	$(MAKE) all
-	$(MAKE) check-valgrind
-	$(MAKE) check-python check-pylint
+	# We don't need to run C unit tests under valgrind for every python
+	# version, run them only when we're testing Python 2.6 as we'll skip
+	# the pylint checks when running it as pylint no longer supports 2.6
+	(python -V 2>&1 | grep -qvF 'Python 2.6') || $(MAKE) check-valgrind
+	(python -V 2>&1 | grep -qF 'Python 2.6') || $(MAKE) check-pylint
+	$(MAKE) check-python
 
 Darwin:
 	brew update
