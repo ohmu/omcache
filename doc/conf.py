55637f051b69863eee745ea265a4aa5c6c5b4c8d
Oskari Saarenmaa <os@ohmu.fi>
2015-02-11 10:37:24 +0200

    doc: get version and release from git describe for readthedocs

--- doc/conf.py
+++ doc/conf.py
@@ -5,6 +5,11 @@ if os.environ.get('READTHEDOCS', None) == 'True':
     subprocess.call(['doxygen'])
     subprocess.call(["git", "clone", "--depth", "1", "--branch", "v3.2.0", "https://github.com/michaeljones/breathe.git", "../breathe-git"])
     sys.path.append("../breathe-git")
+    version = subprocess.check_output(["git", "describe", "--abbrev=0"])
+    release = subprocess.check_output(["git", "describe", "--long"])
+else:
+    version = os.getenv("OMCACHE_VERSION")
+    release = os.getenv("OMCACHE_VERSION_FULL")
 
 # generate index.rst from readme, skip the badges, add toc
 readme_lines = open("../README.rst").read().splitlines()
@@ -25,8 +30,6 @@ Introduction
 open("index.rst", "w").write(beef)
 
 
-version = os.getenv("OMCACHE_VERSION")
-release = os.getenv("OMCACHE_VERSION_FULL")
 project = 'OMcache'
 copyright = '2013-2014, Oskari Saarenmaa'
 
